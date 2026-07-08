923d6f510783011d92f3f53e28c279bda568c7ec
JORED666 <243842@ids.upchiapas.edu.mx>
2026-07-07 20:52:32 -0600

    Eliminar .next del repositorio y agregarlo al .gitignore

--- .next/dev/server/chunks/ssr/_next-internal_server_app_arcade_dados_page_actions_0-v454z.js
+++
@@ -1,7 +0,0 @@
-module.exports = [
-"[project]/.next-internal/server/app/arcade/dados/page/actions.js [app-rsc] (server actions loader, ecmascript)", ((__turbopack_context__, module, exports) => {
-
-}),
-];
-
-//# sourceMappingURL=_next-internal_server_app_arcade_dados_page_actions_0-v454z.js.map
--- .next/dev/server/chunks/ssr/_next-internal_server_app_arcade_page_actions_1dm-wgc.js
+++
@@ -1,7 +0,0 @@
-module.exports = [
-"[project]/.next-internal/server/app/arcade/page/actions.js [app-rsc] (server actions loader, ecmascript)", ((__turbopack_context__, module, exports) => {
-
-}),
-];
-
-//# sourceMappingURL=_next-internal_server_app_arcade_page_actions_1dm-wgc.js.map
